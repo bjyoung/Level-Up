5856b0472fd28cf200c41814e48df3fb665928d3
bjyoung <bjyoung20591@gmail.com>
2022-07-31 21:34:41 -0400

    Change quest list to a fragment

---
+++ app/src/main/java/com/brandonjamesyoung/levelup/QuestList.kt
@@ -0,0 +1,10 @@
+package com.brandonjamesyoung.levelup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class QuestList : Fragment(R.layout.quest_list) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
