bf7da377c5325e661ea94bd311f3f866273aaf2b
bjyoung <bjyoung20591@gmail.com>
2022-07-31 21:35:44 -0400

    Add 'new quest' fragment

---
+++ app/src/main/java/com/brandonjamesyoung/levelup/NewQuest.kt
@@ -0,0 +1,10 @@
+package com.brandonjamesyoung.levelup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class NewQuest : Fragment(R.layout.new_quest) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
