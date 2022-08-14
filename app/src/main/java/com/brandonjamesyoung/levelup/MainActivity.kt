20bf99d39c44120416dc6ad785d390242346c024
bjyoung <bjyoung20591@gmail.com>
2022-08-14 15:38:01 -0400

    Add navigation to Quest List 'Add New' button

--- app/src/main/java/com/brandonjamesyoung/levelup/MainActivity.kt
+++ app/src/main/java/com/brandonjamesyoung/levelup/MainActivity.kt
@@ -2,10 +2,15 @@ package com.brandonjamesyoung.levelup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brandonjamesyoung.levelup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
--- app/src/main/java/com/brandonjamesyoung/levelup/QuestList.kt
+++ app/src/main/java/com/brandonjamesyoung/levelup/QuestList.kt
@@ -1,10 +1,25 @@
 package com.brandonjamesyoung.levelup
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class QuestList : Fragment(R.layout.quest_list) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val addNewQuestButton = view.findViewById<FloatingActionButton>(R.id.AddNewQuestButton)
+
+        addNewQuestButton.setOnClickListener{
+            findNavController().navigate(R.id.action_questList_to_newQuest)
+        }
     }
 }
