3de433285dfc3c6a8fb6d625df653dcf0a2d94ba
bjyoung <bjyoung20591@gmail.com>
2022-08-22 21:25:30 -0400

    Make New Quest cancel button navigate to Quest List

--- app/src/main/java/com/brandonjamesyoung/levelup/NewQuest.kt
+++ app/src/main/java/com/brandonjamesyoung/levelup/NewQuest.kt
@@ -1,7 +1,17 @@
 package com.brandonjamesyoung.levelup
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.brandonjamesyoung.levelup.shared.NavigationHelper
 
 class NewQuest : Fragment(R.layout.new_quest) {
+    private fun addNavigation(view: View){
+        NavigationHelper().addNavigationToView(this, view, R.id.CancelButton, R.id.action_newQuest_to_questList)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addNavigation(view)
+    }
 }
