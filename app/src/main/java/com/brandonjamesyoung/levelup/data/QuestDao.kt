d479eee2a6ba09bb7caea321383629251fa3a834
bjyoung <bjyoung20591@gmail.com>
2022-09-06 19:42:43 -0400

    Update Quest DAO get methods to return Flow
    To improve performance.

--- app/src/main/java/com/brandonjamesyoung/levelup/data/QuestDao.kt
+++ app/src/main/java/com/brandonjamesyoung/levelup/data/QuestDao.kt
@@ -1,14 +1,15 @@
 package com.brandonjamesyoung.levelup.data
 
 import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface QuestDao {
     @Query("SELECT * FROM Quest")
-    fun getAll(): List<Quest>
+    fun getAll(): Flow<List<Quest>>
 
     @Query("SELECT * FROM Quest WHERE id = :id")
-    fun findById(id: Int): Quest
+    fun findById(id: Int): Flow<Quest>
 
     @Insert
     fun insert(quest: Quest)
