c99aebf01fefe2a0e7ad809d8c779847d78ed31a
bjyoung <bjyoung20591@gmail.com>
2022-09-12 21:27:55 -0400

    Add save functionality to New Quest page

--- app/src/main/java/com/brandonjamesyoung/levelup/fragments/NewQuest.kt
+++ app/src/main/java/com/brandonjamesyoung/levelup/fragments/NewQuest.kt
@@ -3,28 +3,40 @@ package com.brandonjamesyoung.levelup.fragments
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import com.brandonjamesyoung.levelup.R
+import com.brandonjamesyoung.levelup.data.Quest
 import com.brandonjamesyoung.levelup.shared.Difficulty
 import com.brandonjamesyoung.levelup.shared.NavigationHelper
 import com.brandonjamesyoung.levelup.shared.Settings
 import com.brandonjamesyoung.levelup.viewmodels.QuestListViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NewQuest : Fragment(R.layout.new_quest) {
-    private val viewModel: QuestListViewModel by viewModels()
-    private var selectedDifficulty: String? = null
-    private var difficultyMap = mapOf(
+    private val viewModel: QuestListViewModel by activityViewModels()
+    private var selectedDifficulty: Difficulty? = null
+
+    private var difficultyToButtonIdMap = mapOf(
         Difficulty.EASY to R.id.EasyButton,
         Difficulty.MEDIUM to R.id.MediumButton,
         Difficulty.HARD to R.id.HardButton,
         Difficulty.EXPERT to R.id.ExpertButton
     )
 
-    private fun addNavigation(view: View){
+    private var buttonIdToDifficultyMap = mapOf(
+        R.id.EasyButton to Difficulty.EASY,
+        R.id.MediumButton to Difficulty.MEDIUM,
+        R.id.HardButton to Difficulty.HARD,
+        R.id.ExpertButton to Difficulty.EXPERT
+    )
+
+    private fun addNavigation(view: View) {
         NavigationHelper.addNavigationToView(
             this,
             view,
@@ -34,7 +46,7 @@ class NewQuest : Fragment(R.layout.new_quest) {
     }
 
     // Move selected difficulty box to the given button
-    private fun moveDifficultySelectBox(button: AppCompatButton, parentView: View){
+    private fun moveDifficultySelectBox(button: AppCompatButton, parentView: View) {
         val constraintLayout = parentView.findViewById<ConstraintLayout>(R.id.NewQuest)
         val constraintSet = ConstraintSet()
         constraintSet.clone(constraintLayout)
@@ -46,29 +58,53 @@ class NewQuest : Fragment(R.layout.new_quest) {
         constraintSet.applyTo(constraintLayout)
     }
 
-    private fun setSelectedDifficulty(buttonId: Int, view: View){
+    private fun setSelectedDifficulty(buttonId: Int, view: View) {
         val button = view.findViewById<AppCompatButton>(buttonId)
-        selectedDifficulty = button.text as String
+        selectedDifficulty = buttonIdToDifficultyMap[buttonId]
         val selectBox = view.findViewById<ImageView>(R.id.DifficultySelectBox)
         selectBox.visibility = ImageView.INVISIBLE
         moveDifficultySelectBox(button, view)
         selectBox.visibility = ImageView.VISIBLE
     }
 
-    private fun setDifficultyButtonListeners(view: View){
-        for (id in difficultyMap.values) {
+    private fun setDifficultyButtonListeners(view: View) {
+        for (id in difficultyToButtonIdMap.values) {
             val button = view.findViewById<AppCompatButton>(id)
 
-            button.setOnClickListener{
+            button.setOnClickListener {
                 setSelectedDifficulty(id, view)
             }
         }
     }
 
-    private fun selectDefaultDifficulty(view: View){
+    private fun createQuest(view: View) {
+        // Get quest data
+        val nameTextView = view.findViewById<TextView>(R.id.NameInput)
+        val questName = nameTextView.text.toString()
+
+        // TODO get icon file here and store in saved quest
+
+        val quest = Quest(
+            name = questName,
+            difficulty = selectedDifficulty!!
+        )
+
+        // Save quest to database
+        viewModel.insert(quest)
+    }
+
+    private fun setSaveButtonListener(view: View) {
+        val saveButton = view.findViewById<AppCompatButton>(R.id.ConfirmButton)
+
+        saveButton.setOnClickListener {
+            createQuest(view)
+        }
+    }
+
+    private fun selectDefaultDifficulty(view: View) {
         // TODO Persist settings in app data
         val settingsObj = Settings()
-        val difficultyButtonId = difficultyMap[settingsObj.DefaultDifficulty]!!
+        val difficultyButtonId = difficultyToButtonIdMap[settingsObj.DefaultDifficulty]!!
         setSelectedDifficulty(difficultyButtonId, view)
     }
 
@@ -76,6 +112,7 @@ class NewQuest : Fragment(R.layout.new_quest) {
         super.onViewCreated(view, savedInstanceState)
         addNavigation(view)
         setDifficultyButtonListeners(view)
+        setSaveButtonListener(view)
         selectDefaultDifficulty(view)
     }
 }
