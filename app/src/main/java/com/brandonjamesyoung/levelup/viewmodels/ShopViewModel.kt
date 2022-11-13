3260b2622e08dc215932efeaa04d7b3f724e6c49
bjyoung <bjyoung20591@gmail.com>
2022-11-12 21:32:42 -0500

    Add basic UI to the Shop page

---
+++ app/src/main/java/com/brandonjamesyoung/levelup/viewmodels/ShopViewModel.kt
@@ -0,0 +1,20 @@
+package com.brandonjamesyoung.levelup.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.brandonjamesyoung.levelup.data.*
+import com.brandonjamesyoung.levelup.di.IoDispatcher
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+private const val TAG = "ShopViewModel"
+
+@HiltViewModel
+class ShopViewModel @Inject constructor(
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val settingsRepository: SettingsRepository,
+) : ViewModel() {
+    val settings: LiveData<Settings> = settingsRepository.observe().asLiveData()
+}
