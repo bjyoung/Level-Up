c0d0d9ad3e2357e7d41631c923392d7d45954878
bjyoung <brandon.j.young80@gmail.com>
2025-03-18 19:15:31 -0400

    Add database indexes
    To speed up queries

--- app/src/main/java/com/brandonjamesyoung/levelup/data/ActiveQuest.kt
+++ app/src/main/java/com/brandonjamesyoung/levelup/data/ActiveQuest.kt
@@ -3,10 +3,11 @@ package com.brandonjamesyoung.levelup.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Index
 import com.brandonjamesyoung.levelup.constants.Difficulty
 import java.time.Instant
 
-@Entity
+@Entity(indices = [Index(value = ["iconId"])])
 data class ActiveQuest(
     @PrimaryKey(autoGenerate = true) override var id: Int = 0,
     @ColumnInfo override var name: String? = null,
--- app/src/main/java/com/brandonjamesyoung/levelup/data/CompletedQuest.kt
+++ app/src/main/java/com/brandonjamesyoung/levelup/data/CompletedQuest.kt
@@ -2,11 +2,12 @@ package com.brandonjamesyoung.levelup.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.brandonjamesyoung.levelup.constants.Difficulty
 import java.time.Instant
 
-@Entity
+@Entity(indices = [Index(value = ["iconId"])])
 data class CompletedQuest(
     @PrimaryKey(autoGenerate = true) override var id: Int = 0,
     @ColumnInfo override var name: String? = null,
